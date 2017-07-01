c50f59c27f07e814d5f21a80ad6a1ac3c904f16b
N415 <andynaisbitt@gmail.com>
2017-07-01 17:22:54 +0100

    Create enableNET35.bat

---
+++ Development/DISM/enableNET35.bat
@@ -0,0 +1,2 @@
+REM CHANGE THE SOURCE TO THE CORRECT PATH ON THE VIRTUAL DISK!!!!!!!!!!!!!!!!!!!@!@!@!@!@!@!@!@
+DISM /Online /Enable-Feature /FeatureName:NetFx3 /All /LimitAccess /Source:f:\x64\sources\sxs

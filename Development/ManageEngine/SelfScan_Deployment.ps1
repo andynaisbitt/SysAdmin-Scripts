115ced545d5cf93c6fdfc6cef09b2ddecbdfde89
N415 <andynaisbitt@gmail.com>
2018-07-05 14:37:13 +0100

    Update SelfScan_Deployment.ps1

--- Development/ManageEngine/SelfScan_Deployment.ps1
+++ Development/ManageEngine/SelfScan_Deployment.ps1
@@ -2,7 +2,6 @@ CD C:\ProgramData\
 MKDIR ManageEngine
 CD ManageEngine
 $client = new-object System.Net.WebClient 
-$client.DownloadFile("https://ithelp.filtronic.com/scanscript/SelfScan.exe","C:\ProgramData\ManageEngine\selfscan.exe")
+$client.DownloadFile("SelfScan.exe","C:\ProgramData\ManageEngine\selfscan.exe")
 $client.DownloadFile("file.xml","C:\ProgramData\ManageEngine\schedule.xml")
-
 Register-ScheduledTask -Xml 'C:\ProgramData\ManageEngine\schedule.xml' -TaskName "ManageEngine SelfScan Task"
